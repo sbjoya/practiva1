406dad58c7582915b00ad90f87ec2b97dd8c0aa0
sbjoya <sbjoya@uninorte.edu.co>
2021-09-21 07:49:14 -0500

    Se adiciona dato

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -4,6 +4,8 @@ public class GeneradorVertebrados {
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
         miVertebrados[2] = new Vertebrado("Perro", "Pez");
+        miVertebrados[3] = new Vertebrado("Pug", "Perro");
+
 
         return miVertebrados;
     }
